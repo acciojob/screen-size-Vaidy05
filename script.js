8fc078360f8b53db8f89c40e8c7a630856db40e9
Vaidy05 <92988611+Vaidy05@users.noreply.github.com>
2023-04-16 15:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+
+const item = document.getElementById("sizeInfo");
+window.onload = function(){
+		item.innerHTML="Width: "+screen.width+" and Height: "+screen.height;
+	};
+window.onresize = function(){
+		item.innerHTML="Width: "+screen.width+" and Height: "+screen.height;
+	};

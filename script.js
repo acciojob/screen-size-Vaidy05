2c6947e729769845dc7b6f1fa1946c443c7777f4
Vaidy05 <92988611+Vaidy05@users.noreply.github.com>
2023-04-16 15:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const expectedWidth = window.innerWidth;
 const expectedHeight = window.innerHeight;
 const item = document.querySelector("#sizeInfo h1");
-window.onload = new ResizeObserver(() => {
+addEventListener("resize",resize);
+function resize(){
 		item.innerHTML="Width: "+expectedWidth+" and Height: "+expectedHeight;
-	})
-let resizeObserver = new ResizeObserver(() => {
-		item.innerHTML="Width: "+expectedWidth+" and Height: "+expectedHeight;
-	})
-resizeObserver.observe(document.getElementsByTagName("body")[0]);
+};
+window.onload = resize;
+window.onresize = resize;
+
